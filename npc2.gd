f1fafa0974efefa36acf6a37e8395d98c623046d
Chrisjoi503 <christinajou125@gmail.com>
2025-05-25 05:51:19 -0400

    ui dialogue

--- npc2.gd
+++
@@ -1,9 +0,0 @@
-extends StaticBody3D
-
-func _ready() -> void:
-	$InteractionDetector.player_interacted.connect(when_interacted)
-
-func when_interacted(body):
-	print("interacted")
-	Dialogic.start("bass_timeline")
-	
